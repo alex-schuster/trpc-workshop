62e04186f94af88b0e9af17049ce4543166b72ce
Alexander Schuster <it201516@fhstp.ac.at>
2023-03-23 11:39:20 +0100

    Workshop starter project setup

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,16 +1,10 @@
 import { createTRPCRouter } from "~/server/api/trpc";
-import { flightsRouter } from "~/server/api/routers/flights";
-import { airportsRouter } from "./routers/airports";
-import { passengersRouter } from "./routers/passengers";
 /**
  * This is the primary router for your server.
  *
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  flights: flightsRouter,
-  airports: airportsRouter,
-  passengers: passengersRouter,
 });
 
 // export type definition of API
--- src/server/api/routers/airports.ts
+++
@@ -1,9 +0,0 @@
-import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
-
-const findAirports = publicProcedure.query(async ({ ctx }) => {
-  return await ctx.prisma.airport.findMany();
-});
-
-export const airportsRouter = createTRPCRouter({
-  findAirports,
-});
